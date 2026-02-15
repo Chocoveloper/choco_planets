896ce4d6174f01c651284df4c2eaacb121cc14e5
Juan Hurtado <maniveloper@gmail.com>
2026-02-15 18:21:02 -0500

    v1.3.0: StatefulWidget and responsive desing implemented with MediaQuery and ConstrainedBox

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
+class MyApp extends StatefulWidget {
 
-class MyApp extends StatelessWidget {
-  static final List<String> planets = [
+  
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+
+  final size = MediaQuery.of(context).size.width;
+
+    final List<String> planets = [
     'Mercury',
     'Venus',
     'Earth',
@@ -14,22 +27,21 @@ class MyApp extends StatelessWidget {
     'Neptune',
     'Pluto',
   ];
-
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Choco Planets',
       home: Scaffold(
         appBar: AppBar(centerTitle: true, title: const Text('Choco Planets')),
         body: Center(
-          child: ListView.builder(
-            itemCount: planets.length,
-            itemBuilder: (context, index){
-              return Text(planets[index]);
-            },
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 600.0),
+            child: ListView.builder(
+              padding: EdgeInsets.symmetric(horizontal: size * 0.1, vertical: 25.0),
+              itemCount: planets.length,
+              itemBuilder: (context, index) {
+                return Text(planets[index]);
+              },
+            ),
           ),
         ),
       ),
