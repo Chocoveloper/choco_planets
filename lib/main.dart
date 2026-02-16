a19a8b401193ddcbe20cee380acd095fcbe693d9
Juan Hurtado <maniveloper@gmail.com>
2026-02-15 20:55:16 -0500

    v1.4.0: Se agrega interactividad con SetState y FloatigActionButton para agregar planetas a lista planets desde otra lista y usando una logica sugerida or Gemini

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
-class MyApp extends StatefulWidget {
 
-  
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+        body: ChocoHome(),
+      ),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class ChocoHome extends StatefulWidget {
+  const ChocoHome({super.key});
+
   @override
-  Widget build(BuildContext context) {
+  State<ChocoHome> createState() => _ChocoHomeState();
+}
 
-  final size = MediaQuery.of(context).size.width;
-
-    final List<String> planets = [
+class _ChocoHomeState extends State<ChocoHome> {
+  final List<String> planets = [
     'Mercury',
     'Venus',
     'Earth',
@@ -27,16 +36,30 @@ class _MyAppState extends State<MyApp> {
     'Neptune',
     'Pluto',
   ];
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Choco Planets',
-      home: Scaffold(
+  final List<String> newPlanets = [
+    'Aiur',
+    'Char',
+    'Kaldir',
+    'Korhal IV',
+    'Mar Sara',
+    'Moria',
+    'Skygeirr',
+    'Tarsonis',
+    'Zerus',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size.width;
+    return Scaffold(
         appBar: AppBar(centerTitle: true, title: const Text('Choco Planets')),
         body: Center(
           child: ConstrainedBox(
             constraints: const BoxConstraints(maxWidth: 600.0),
             child: ListView.builder(
-              padding: EdgeInsets.symmetric(horizontal: size * 0.1, vertical: 25.0),
+              padding: EdgeInsets.symmetric(
+                horizontal: size * 0.1,
+                vertical: 25.0,
+              ),
               itemCount: planets.length,
               itemBuilder: (context, index) {
                 return Text(planets[index]);
@@ -44,7 +67,45 @@ class _MyAppState extends State<MyApp> {
             ),
           ),
         ),
-      ),
-    );
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              //Este botón añade la frase hay un nuevo Choco-Planeta y continua el conteo
+              onPressed: () {
+                setState(() {
+                  planets.add(
+                    'Hay un nuevo Choco-Planeta ${planets.length + 1}',
+                  );
+                });
+              },
+              heroTag: 'ChocoPlanets',
+              child: Icon(Icons.add),
+            ),
+            const SizedBox(width: 10.0),
+            FloatingActionButton(
+              //Este botón añade un nuevo planeta de la lista newPlanets
+              //Usando una condición if para que no se repita ningún planeta
+              //Y cuando ya no existan más planetas muestre un mensaje al usuario a través de un SnackBar
+              onPressed: () {
+                setState(() {
+                  if (newPlanets.isNotEmpty) {
+                    String planetaExtraido = newPlanets.removeAt(0);
+                    planets.add(planetaExtraido);
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('No hay más planetas para añadir'),
+                      ),
+                    );
+                  }
+                });
+              },
+              heroTag: 'Sector Kropulu',
+              child: Icon(Icons.abc),
+            ),
+          ],
+        ),
+      );
   }
 }
